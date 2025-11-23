f8a80cd1982517315e89e64d342c972b97499bc2
Kaan Kılıçarslan <kaanklcrsln@Kaans-MacBook-Air.local>
2025-11-24 00:11:49 +0300

    add base path for asset loading

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './', // Assetlerin yüklenmeme sorununu çözer
   server: {
     port: 3000
   },
