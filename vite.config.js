e55cef95e712cf609c3a826ffdb43507b4be6d65
Kaan Kılıçarslan <kaanklcrsln@Kaans-MacBook-Air.local>
2025-11-24 20:25:18 +0300

    Fix CSS errors, add DIN-Next-Rounded font, optimize build with code splitting

--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,17 @@ export default defineConfig(({ command }) => {
     build: {
       outDir: 'dist',
       assetsDir: 'assets',
+      minify: 'esbuild',
       rollupOptions: {
         output: {
-          assetFileNames: 'assets/[name]-[hash][extname]'
+          assetFileNames: 'assets/[name]-[hash][extname]',
+          manualChunks: {
+            vendor: ['react', 'react-dom'],
+            openlayers: ['ol', 'ol/proj', 'ol/source'],
+          }
         }
-      }
+      },
+      chunkSizeWarningLimit: 1000,
     },
     experimental: {
       renderBuiltUrl(filename, { hostType }) {
